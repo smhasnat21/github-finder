8b7b0eb75c5fd4855c23a6b8fa3372881e6922a9
S. M. HASNAT HOSSAIN <75869317+smhasnat21@users.noreply.github.com>
2022-06-04 19:23:45 +0600

    Update About.js
    Deleted h3

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,6 @@ import React, { Fragment } from 'react'
 const About = () => {
   return (
     <Fragment>
-      <h6>Design | Developed</h6>
       <h3>S. M. Hasnat Hossain</h3>
       <h5>Full Stack Developer</h5>
       <p>App to search Github users</p>
