b1b0830473f4d110db2658b2df3bd25d673d1b83
S M Hasnat Hossain <smhasnat21@gmail.com>
2022-06-04 12:03:52 +0600

    github search

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+      <h6>Design | Developed</h6>
+      <h3>S. M. Hasnat Hossain</h3>
+      <h5>Full Stack Developer</h5>
+      <p>App to search Github users</p>
+      <p>Version: 3.06.22</p>
+    </Fragment>
+  )
+}
+
+export default About
